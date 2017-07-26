db38a20c2a25718faeb0eea9ac14e2a1cc78d706
转二 <gaofeng.gf@alibaba-inc.com>
2017-07-26 14:44:14 +0800

    Initial Commit

---
+++ SketchPluginBase/SketchPluginBase.h
@@ -0,0 +1,19 @@
+//
+//  SketchPluginBase.h
+//  SketchPluginBase
+//
+//  Created by gaofeng.gf on 2017/7/26.
+//  Copyright © 2017年 gaofeng.gf. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SketchPluginBase.
+FOUNDATION_EXPORT double SketchPluginBaseVersionNumber;
+
+//! Project version string for SketchPluginBase.
+FOUNDATION_EXPORT const unsigned char SketchPluginBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SketchPluginBase/PublicHeader.h>
+
+
